87cbf4089530917ce034dab2efa49a80b9a95a5e
MagmaMusen <MagmaMusen@gmail.com>
2020-03-10 09:50:36 +0100

    Unfinished events for door and added station control

---
+++ LadeSkab/DoorEventArgs.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LadeSkab
+{
+    public class DoorEventArgs : EventArgs
+    {
+        public string DoorStatus { set; get; }
+    }
+}
