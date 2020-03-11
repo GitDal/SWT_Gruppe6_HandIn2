08ecb43b402999c44113adda1cb712f03a3a4973
GitDal <jeppedybdal@gmail.com>
2020-03-11 15:38:37 +0100

    Created the logfile implementation

--- LadeSkab/LogFile.cs
+++ LadeSkab/LogFile.cs
@@ -1,21 +1,60 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace LadeSkab
 {
-    public class LogFile
+    public class LogFile: ILogger
     {
-        void LogDoorLocked(string id)
+        private string path;
+        private string fileName;
+
+        public LogFile(string filename)
         {
+            fileName = filename;
+            path = Directory.GetCurrentDirectory() + @"\" + fileName;
+        }
+
+        public void LogDoorLocked(string id)
+        {
+            if (!File.Exists(path))
+            {
+                using (StreamWriter logWriterInit = File.CreateText(path))
+                {
+                    logWriterInit.WriteLine($"Logging of door openings and closings");
+                    logWriterInit.WriteLine($"ID: {id} - Locked door at: {DateTime.Now.ToString()}");
+                }
+            }
+            else
+            {
+                using (StreamWriter logWriter = File.AppendText(path))
+                {
+                    logWriter.WriteLine($"ID: {id} - Locked door at: {DateTime.Now.ToString()}");
+                }
+            }
 
         }
 
-        void LogDoorUnlocked(string id)
+        public void LogDoorUnlocked(string id)
         {
-
+            if (!File.Exists(path))
+            {
+                using (StreamWriter logWriterInit = File.CreateText(path))
+                {
+                    logWriterInit.WriteLine($"Logging of door openings and closings");
+                    logWriterInit.WriteLine($"ID: {id} - Unlocked door at: {DateTime.Now.ToString()}");
+                }
+            }
+            else
+            {
+                using (StreamWriter logWriter = File.AppendText(path))
+                {
+                    logWriter.WriteLine($"ID: {id} - Unlocked door at: {DateTime.Now.ToString()}");
+                }
+            }
         }
     }
 }
