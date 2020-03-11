686330d92043b6495dff7b595885683f11bb1df4
GitDal <jeppedybdal@gmail.com>
2020-03-11 14:31:09 +0100

    Added handout code

---
+++ LadeSkab/IChargeControl.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LadeSkab
+{
+    public interface IChargeControl
+    {
+        void TelephoneConnected();
+
+        // Event triggered on new current value
+        event EventHandler<CurrentEventArgs> CurrentValueEvent;
+
+        // Direct access to the current current value
+        double CurrentValue { get; }
+
+        // Require connection status of the phone
+        bool IsConnected { get; }
+
+        // Start charging
+        void StartCharge();
+        // Stop charging
+        void StopCharge();
+    }
+}
