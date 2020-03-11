00cdbca4c2ee1144dc2737de8e044e088ad3904c
MagmaMusen <MagmaMusen@gmail.com>
2020-03-11 15:59:00 +0100

    Started on test

--- LadeSkab_Test/LadeSkabUnitTests.cs
+++ LadeSkab_Test/LadeSkabUnitTests.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
+using NSubstitute;
+using LadeSkab;
 
 namespace LadeSkab_Test
 {
-    public class Class1
+    [TestFixture]
+    class DoorUnitTests
     {
+        
+
+
     }
 }
