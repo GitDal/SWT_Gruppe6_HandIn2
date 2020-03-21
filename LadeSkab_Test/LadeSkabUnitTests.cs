a870f630545e343068162ec5efdf03889cd7d283
GitDal <jeppedybdal@gmail.com>
2020-03-21 17:46:22 +0100

    Test til display funktionerne - Meget trivielle og fejler hurtigt hvis teksten i funktionerne ændrer sig

--- LadeSkab_Test/LadeSkabUnitTests.cs
+++ LadeSkab_Test/LadeSkabUnitTests.cs
@@ -601,6 +601,80 @@ namespace LadeSkab_Test
 
             Assert.That(_output.ToString(), Is.EqualTo("On Display: '" + msg + "'\r\n"));
         }
+
+        [Test]
+        public void ShowOverload_Called_MessageIsWrittenCorrectlyOnScreen()
+        {
+            _uut.ShowOverload();
+
+            Assert.That(_output.ToString(), Is.EqualTo("On Display: '" + "Error during charging - Remove device immediately" + "'\r\n"));
+        }
+
+        [Test]
+        public void ShowConnectDevice_Called_MessageIsWrittenCorrectlyOnScreen()
+        {
+            _uut.ShowConnectDevice();
+
+            Assert.That(_output.ToString(), Is.EqualTo("On Display: '" + "Please connect device." + "'\r\n"));
+        }
+
+        [Test]
+        public void ShowConnectionError_Called_MessageIsWrittenCorrectlyOnScreen()
+        {
+            _uut.ShowConnectionError();
+
+            Assert.That(_output.ToString(), Is.EqualTo("On Display: '" + "Connection Error: Your device is not properly connected. Try again." + "'\r\n"));
+        }
+
+        [Test]
+        public void ShowFullyCharged_Called_MessageIsWrittenCorrectlyOnScreen()
+        {
+            _uut.ShowFullyCharged();
+
+            Assert.That(_output.ToString(), Is.EqualTo("On Display: '" + "Phone Fully Charged" + "'\r\n"));
+        }
+
+        [Test]
+        public void ShowDeviceCharging_Called_MessageIsWrittenCorrectlyOnScreen()
+        {
+            _uut.ShowDeviceCharging();
+
+            Assert.That(_output.ToString(), Is.EqualTo("On Display: '" + "Phone Charging" + "'\r\n"));
+        }
+
+        [Test]
+        public void ShowOccupied_Called_MessageIsWrittenCorrectlyOnScreen()
+        {
+            _uut.ShowOccupied();
+
+            Assert.That(_output.ToString(), Is.EqualTo("On Display: '" + "Occupied: Door is locked and your device is charging.Use your RFID tag to unlock door." + "'\r\n"));
+        }
+
+        [Test]
+        public void ShowProvideId_Called_MessageIsWrittenCorrectlyOnScreen()
+        {
+            _uut.ShowProvideId();
+
+            Assert.That(_output.ToString(), Is.EqualTo("On Display: '" + "Provide RFID to lock." + "'\r\n"));
+        }
+
+        [Test]
+        public void ShowRemoveDevice_Called_MessageIsWrittenCorrectlyOnScreen()
+        {
+            _uut.ShowRemoveDevice();
+
+            Assert.That(_output.ToString(), Is.EqualTo("On Display: '" + "Remove device." + "'\r\n"));
+        }
+
+        [Test]
+        public void ShowWrongId_Called_MessageIsWrittenCorrectlyOnScreen()
+        {
+            _uut.ShowWrongId();
+
+            Assert.That(_output.ToString(), Is.EqualTo("On Display: '" + "Wrong RFID tag. Use your RFID tag to try again." + "'\r\n"));
+        }
+
+
     }
 
 
